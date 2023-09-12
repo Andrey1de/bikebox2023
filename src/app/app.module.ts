735b50d17d3ee5cfb49ed69fde82a58cca8e060b
andrey1de <andrey1de@gmail.com>
2023-09-12 04:05:47 +0300

    333

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainFrameComponent } from './main-frame/main-frame.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainFrameComponent
   ],
   imports: [
     BrowserModule
