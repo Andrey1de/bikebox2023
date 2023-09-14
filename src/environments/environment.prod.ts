89abf7797f3c5f7f51418f8b9acf95828f9402d0
andrey1de <andrey1de@gmail.com>
2023-09-15 02:45:13 +0300

    Begin 1

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: true,
+    version: '1.1.2',
+    apiUrl: 'http://localhost:4000',
+    apiRandomUser: 'http://randomuser.me/api',
+    fileUsers:  "users.json",
+    isMOK:true,
+    toTestUsers:true,
+    toBlurKey:true,
+    storePrefix : 'storeUserAbiber16',
+    storeMap  :  'storeUserAbiber16-map', 
+    isRussian: false,
+};
